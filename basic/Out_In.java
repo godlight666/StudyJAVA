9faf03ff31cc97c6b8414c099c47b5436b4ba41d
godlight <1220805215@qq.com>
2020-03-17 23:41:52 +0800

    add Out_In

---
+++ basic/Out_In.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Out_In{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input your name: ");
+        String name = scanner.nextLine();
+        scanner.close();
+    }
+    
+}
